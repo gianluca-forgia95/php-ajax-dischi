5d61afcd3cb6e687f2f77cfa6bc762908d5bd16e
Gianluca Forgia <gianluca.forgia@gmail.com>
2021-05-05 17:01:39 +0200

    fine esercizio

--- js/app.js
+++ js/app.js
@@ -7,6 +7,11 @@ var root = new Vue({
          axios.get('http://localhost/php-ajax-dischi/api.php')
          .then( (resp) => {
             this.albums = resp.data;
+         
+         //Ordino gli Album per anno di uscita( dall'anno di uscita piu recente al piu vecchio )
+         this.albums.sort( ( olderAlbum , youngerAlbum ) => {
+           return youngerAlbum.year - olderAlbum.year;
+         });
     
          });
     
