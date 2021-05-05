554b17ea2192afae7f84a215484a443e63e287d6
Gianluca Forgia <gianluca.forgia@gmail.com>
2021-05-05 15:38:22 +0200

    separo l'head in un altro file php e lo includo

---
+++ head.php
@@ -0,0 +1,17 @@
+<head>
+    <meta charset="utf-8">
+    <!-- Google Font -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&display=swap" rel="stylesheet">
+    <!-- /Google Font -->
+    <!-- Axios -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.js" integrity="sha512-otOZr2EcknK9a5aa3BbMR9XOjYKtxxscwyRHN6zmdXuRfJ5uApkHB7cz1laWk2g8RKLzV9qv/fl3RPwfCuoxHQ==" crossorigin="anonymous"></script>
+    <!-- /Axios -->
+    <!-- VueJs -->
+    <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+    <!-- /VueJs -->
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- /CSS -->
+    <title>Dischi</title>
+  </head>
